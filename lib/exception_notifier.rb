1d6ec3f5db3122774f04fb950df87200a48bf2ce
Jamis Buck <jamis@37signals.com>
2007-11-23 16:26:45 +0000

    be explicit about the content type of exception notification emails (closes #9985)

--- lib/exception_notifier.rb
+++ lib/exception_notifier.rb
@@ -36,6 +36,8 @@ class ExceptionNotifier < ActionMailer::Base
   def self.reloadable?; false; end
 
   def exception_notification(exception, controller, request, data={})
+    content_type "text/plain"
+
     subject    "#{email_prefix}#{controller.controller_name}##{controller.action_name} (#{exception.class}) #{exception.message.inspect}"
 
     recipients exception_recipients
